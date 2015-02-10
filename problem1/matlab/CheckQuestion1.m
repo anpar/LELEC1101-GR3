8982d89f9b7930ab26a45716ab2ea083adf535fe
Antoine Paris <paris.antoine.paris@gmail.com>
2015-02-10 16:48:22 +0100

    Improve and add doc

--- problem1/matlab/CheckQuestion1.m
+++ problem1/matlab/CheckQuestion1.m
@@ -1,19 +1,24 @@
-function [] = CheckQuestion1(Vmin,Vmax,T,animation, pauseTime, limit)
-k=1;
-a=0;
+% INPUT :
+% * Vmin et Vmax : amplitude minimale et maximale du signal
+% * T : période du signal
+% * anim : si true, affiche l'animation du signal, si false,
+% affiche juste le résultat final
+% * pauseDuration : temps de la pause pour l'animation
+% * limit : limite du nombre k
+function [] = CheckQuestion1(Vmin,Vmax,T,anim, pauseDuration, limit)
 t = linspace(0,4*T,1000);
 u = zeros(1,1000);
-omega = 2*pi/T;
-for i=1:limit;
-    u = u + (8/(T^2 *omega^2*i^2))*(Vmax-Vmin)*(1-((-1)^i))*cos(i*omega*t);
-    if animation == true
-        plot (t,u); 
-        str = sprintf('k = %d/%d',i,limit);
+w = 2*pi/T;
+for k=1:limit;
+    u = u + (8/(T^2 *w^2*k^2))*(Vmax-Vmin)*(1-((-1)^k))*cos(k*w*t);
+    if anim == true
+        plot (t,u);  
+        str = sprintf('k = %d/%d',k,limit);
         title(str)
-        pause(pauseTime);
+        pause(pauseDuration);
     end
 end
-if animation == false 
+if anim == false 
     plot(t,u)
     ylabel('amplitude [V]') 
     xlabel('temps [s]')
