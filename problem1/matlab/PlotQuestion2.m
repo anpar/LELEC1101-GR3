a67a86f82f7d2b8257b5c84ab84e99aded94dfe9
anpar <paris.antoine.paris@gmail.com>
2015-02-10 23:35:45 +0100

    Add beginning of question2

--- problem1/matlab/PlotQuestion2.m
+++ problem1/matlab/PlotQuestion2.m
@@ -1,11 +1,22 @@
-% INPUT : s, valeur du paramètre de la
-% fonction non linéaire.
-% OUTPUT : un graphe de la fonction entre -8 et 8
+% INPUT : s, valeur du parametre de la
+% fonction non lineaire.
+% OUTPUT : un graphe de la fonction entre -10 et 10
 % avec 1000 points.
 function [] = PlotQuestion2(s)
-    x = linspace(-8,8,1000);
+    x = linspace(-10,10,1000);
     h = x./(sqrt((x/s).^2+1));
     plot(x,h)
     xlabel('input')
     ylabel('output')
 end
+
+% Code used to create the figure in the report
+% figure
+% subplot(2,2,1)
+% PlotQuestion2(1)
+% subplot(2,2,2)
+% PlotQuestion2(2)
+% subplot(2,2,3)
+% PlotQuestion2(3)
+% subplot(2,2,4)
+% PlotQuestion2(4)
