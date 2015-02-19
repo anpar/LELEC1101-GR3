7981a95927b9c2b38e65617263fd1d495d3b1ef4
anpar <paris.antoine.paris@gmail.com>
2015-02-19 22:16:46 +0100

    question2 almost finished

--- problem1/matlab/PlotQuestion2.m
+++ problem1/matlab/PlotQuestion2.m
@@ -3,9 +3,12 @@
 % OUTPUT : un graphe de la fonction entre -10 et 10
 % avec 1000 points.
 function [] = PlotQuestion2(s)
-    x = linspace(-10,10,1000);
+    x = linspace(-3*s,3*s,1000);
     h = x./(sqrt((x/s).^2+1));
-    plot(x,h)
+    plot(x,h,'b',x,x-(x.^3/(2*s^2))+(3*x.^5)/(8*s^4),'g');
+    %plot(x,x); hold on;
+    %plot(x,x-(x^3/(2*s^2))); hold on;
+    %plot(x,x-(x^3/(2*s^2))+(3*x^5)/(8*s^4),'g')
     xlabel('input')
     ylabel('output')
 end
