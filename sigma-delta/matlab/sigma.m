c0520a082a07a1ddcd5016b142e452e35e803201
anpar <paris.antoine.paris@gmail.com>
2015-03-24 09:19:51 +0100

    Fin du dimensionnement du sigma-delta

--- sigma-delta/matlab/sigma.m
+++ sigma-delta/matlab/sigma.m
@@ -1,18 +1,17 @@
 figure();
 xlim([-15 15])
-ylim([0 80000])
 vref = linspace(-15,15,1000);
 Vee = -15;
-E = 15;
-C1 = 6e-9;
-R1 = 5000;
-R2 = 50000;
-R3 = 10000;
-R4 = 20000;
-R6 = 20000;
-K = 1/(R1*C1);
+E = 13.5;
+C1 = 1.5e-9;
+R1 = 10000;
+R2 = 82000+47000+5600;
+R3 = 22000;
+R4 = 47000+1500;
+R6 = 47000+1500;
+K = 1/(R3*C1)
 DV = R1/R2 * E;
 VREF = -R3*(vref/R4 + Vee/R6);
 f = -K/DV * (VREF.*(VREF-E))/E;
-plot(vref,f)
+plot(vref,f); grid;
 % Pas mal mais à refaire avec E = 13.5 (plus proche de la réalité)
